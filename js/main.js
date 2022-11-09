e5b7d6d04216d8980e226974a2e73c8f0d297ad4
TrixyParra <trixy.parra@gmail.com>
2022-11-08 22:43:00 -0800

    Created/added the functions for the buttons and added them to be called on the button onclick. Added some styling to the scoreboard score width and a margin to the buttons.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
 // ToDo: Basketball Scoreboard 
 
+// * Global Variables 
+const homeScore = document.getElementById("home-score"); 
+const guestScore = document.getElementById("guest-score"); 
+
+let homePoints = 0; 
+let guestPoints = 0; 
+
+// * Plus Points Functions 
+// Home Points Functions 
+function plus1PointHome() {
+    homePoints += 1 
+    homeScore.textContent = homePoints 
+} 
+
+function plus2PointsHome() {
+    homePoints += 2 
+    homeScore.textContent = homePoints 
+} 
+
+function plus3PointsHome() {
+    homePoints += 3 
+    homeScore.textContent = homePoints 
+} 
+
+// Guest Points Functions 
+function plus1PointGuest() {
+    guestPoints += 1 
+    guestScore.textContent = guestPoints 
+} 
+
+function plus2PointsGuest() {
+    guestPoints += 2 
+    guestScore.textContent = guestPoints 
+} 
+
+function plus3PointsGuest() {
+    guestPoints += 3 
+    guestScore.textContent = guestPoints 
+} 
