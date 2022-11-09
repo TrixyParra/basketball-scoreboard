f2edcd43e8bf98a5170b317f7676d07b386f30af
TrixyParra <trixy.parra@gmail.com>
2022-11-08 22:54:19 -0800

    Created/added new game function to work on click of the new game button.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const guestScore = document.getElementById("guest-score");
 let homePoints = 0; 
 let guestPoints = 0; 
 
+
 // * Plus Points Functions 
 // Home Points Functions 
 function plus1PointHome() {
@@ -38,4 +39,16 @@ function plus2PointsGuest() {
 function plus3PointsGuest() {
     guestPoints += 3 
     guestScore.textContent = guestPoints 
+} 
+
+
+// * New Game Function 
+function newGame() {
+    // clears scoreboard to display 0 
+    homeScore.textContent = 0 
+    guestScore.textContent = 0 
+
+    // begins new count from 0 
+    homePoints = 0 
+    guestPoints = 0 
 } 
